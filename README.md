0cadc8bcd7345eb559353a34c9b395e3b6040a7b
JosephPshine63 <122402946+JosephPshine63@users.noreply.github.com>
2024-03-20 10:28:30 +0100

    tre

--- README.md
+++
@@ -1,2 +0,0 @@
-# Tutorials
-Fullstack application where you can insert a tutorial and view the registered ones, even edit them
--- src/main/java/dev/pioruocco/repository/TutorialRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.pioruocco.repository;
-
-import dev.pioruocco.domain.entity.TutorialEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TutorialRepository extends JpaRepository<TutorialEntity, Integer> {
-}
